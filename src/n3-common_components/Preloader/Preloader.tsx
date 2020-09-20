8061ca2ccfb54d6aa4bb82230ae6765145958b63
colorfulmdx2 <colorfulmdx2@gmail.com>
2020-09-20 19:20:18 +0300

    Registration page

---
+++ src/n3-common_components/Preloader/Preloader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import preloader from "../../n2-assets/images/preloader.gif"
+
+export const Preloader = () => {
+    return (
+        <div>
+            <img src={preloader}/>
+        </div>
+    )
+}
