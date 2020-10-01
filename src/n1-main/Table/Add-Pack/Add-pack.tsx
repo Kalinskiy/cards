5e8e83164665c00e8e7687a92dc427e3d4f3e643
sasha.kalinskiy@yandex.ru <jsn364cmd025>
2020-10-01 03:10:06 +0300

    add clearing after clicking submit button in Table

--- src/n1-main/Table/Add-Pack/Add-pack.tsx
+++ src/n1-main/Table/Add-Pack/Add-pack.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {useFormik} from "formik";
-import {registrationTC} from "../../Registration-Page/Reducer/RegistrationReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {addPackTC} from "../Reducer/TableReducer";
 import {AppStateType} from "../../m2-bll/store";
+import s from './Add-pack.module.css'
+
+
 
 export const AddPackForm = () => {
 
@@ -11,24 +13,36 @@ export const AddPackForm = () => {
     const userId = useSelector<AppStateType, string | null>(state => state.login.auth._id)
 
     const formik = useFormik({
-        validate: (values) => {},
+        validate: (values) => {
+        },
         initialValues: {
             name: '',
         },
-        onSubmit: values => {
+        onSubmit: (values, {resetForm}:any) => {
             dispatch(addPackTC(userId, values))
+            resetForm({values:''})
+
+
+
         }
+
     })
+
 
     return (
         <form onSubmit={formik.handleSubmit}>
-            <label htmlFor={"name"}>Card Name</label>
+            <div className={s.cardName}>
+                <label htmlFor={"name"}>Card Name</label>
+                <input
+                    type="text"
+                    {...formik.getFieldProps("name")}
+                />
+                <button type="submit"   >Submit</button>
+            </div>
+
             <br/>
-            <input
-                type="text"
-                {...formik.getFieldProps("name")}
-            />
-            <button type="submit">Submit</button>
+
+
         </form>
     )
 }
